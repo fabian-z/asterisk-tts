e6d96daf2ce4f475002f01a5adf0780ba7567fbd
Fabian Zaremba <fabian@youremail.eu>
2018-11-05 13:18:51 +0100

    Rework fetch script

--- res/fetch.sh
+++ res/fetch.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-wget https://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
-wget https://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
-tar xvf asterisk-core-sounds-en-wav-current.tar.gz core-sounds-en.txt
-tar xvf asterisk-extra-sounds-en-wav-current.tar.gz extra-sounds-en.txt
+
+set -e
+
+URL="https://downloads.asterisk.org/pub/telephony/sounds"
+CORE="asterisk-core-sounds-en-wav-current"
+EXTRA="asterisk-extra-sounds-en-wav-current"
+
+rm -rf "$CORE" && rm -rf "$EXTRA" 
+mkdir "$CORE" && mkdir "$EXTRA"
+
+wget "$URL/$CORE.tar.gz"
+wget "$URL/$EXTRA.tar.gz"
+
+cd "$CORE"
+tar xvf "../$CORE.tar.gz"
+
+cd ..
+cd "$EXTRA"
+tar xvf "../$EXTRA.tar.gz"
+
+cd ..
+cp "$CORE/core-sounds-en.txt" ./
+cp "$EXTRA/extra-sounds-en.txt" ./
+
